46d54825c5b7b3d71e04f273757bb99d68d8d41b
Ihor Ivlev <ivlev.igor@gmail.com>
2020-06-05 15:27:34 +0300

    Booblean comparison safety for Result<T>

--- lib/include/Result.hpp
+++ lib/include/Result.hpp
@@ -13,6 +13,11 @@ enum class Error {
 };
 
 template <typename T> class Result {
+    // safe-bool idiom - avoid integer conversions
+    // https://www.artima.com/cppsource/safebool.html
+    using bool_type = void (Result<T>::*)() const;
+    void this_type_does_not_support_comparisons() const {}
+
   public:
     explicit Result(T value);
     Result(Error value);
@@ -20,13 +25,25 @@ template <typename T> class Result {
     inline T& value();
     inline const Error& error() const;
 
-    inline operator bool() const;
+    inline operator bool_type() const;
 
   private:
     inline bool hasError() const;
 
     std::variant<T, Error> m_result;
 };
+
+template <typename T, typename Other>
+bool operator!=(const Result<T> lhs, const Other& rhs) {
+    lhs.this_type_does_not_support_comparisons();
+    return false;
+}
+
+template <typename T, typename Other>
+bool operator==(const Result<T> lhs, const Other& rhs) {
+    lhs.this_type_does_not_support_comparisons();
+    return false;
+}
 
 template <typename T> Result<T>::Result(T value) { m_result = value; }
 
@@ -42,6 +59,9 @@ template <typename T> bool Result<T>::hasError() const {
     return std::holds_alternative<Error>(m_result) && error() != Error::NoError;
 }
 
-template <typename T> Result<T>::operator bool() const { return !hasError(); }
+template <typename T> Result<T>::operator bool_type() const {
+    return !hasError() ? &Result<T>::this_type_does_not_support_comparisons
+                       : nullptr;
+}
 
 } // namespace MyOpenSslExample
